2b18287869701d117c7a673b9d910385e3647d36
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-05-23 22:43:08 +0530

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/header.css'
+import { getAuth } from 'firebase/auth';
+
+
+// if user logged in hide the buttons and if not unhide the buttons 
+// if user looged in  inside of the button render profile and logout 
+// if user not logged in show signup and login button and route them!
+
+
+const Header = () => {
+
+    return (
+        <>
+        </>
+    );
+};
+
+
+export default Header;
--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -22,10 +22,24 @@ const SplashScreen = () => {
     textAlign: 'center',
   };
 
+  const containerStyle2 = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh',
+    textAlign: 'center',
+  };
+
     return (
-     <div style={containerStyle}>
+    <>
+    <div style={containerStyle2}>
+    <div style={containerStyle}>
       <Lottie options={defaultOptions}/>
     </div>
+    </div>
+    </>
   );
 };
 
