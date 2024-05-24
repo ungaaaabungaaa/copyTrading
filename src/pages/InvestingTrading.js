09f0a4cee3a43291b5883fca8ef6d332cdde528b
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-05-24 20:40:24 +0530

    added investing page

--- src/pages/InvestingTrading.js
+++ src/pages/InvestingTrading.js
@@ -2,16 +2,51 @@ import React from 'react';
 import '../styles/global.css';
 import Footer from '../components/footer';
 import Header from '../components/Header'
+import '../styles/invest.css'
+import Lottie from 'react-lottie';
+import animationData from '../assets/Animations/invest.json';
 
-function investingTrading()
+function InvestingTrading()
 {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+    
+      const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '72%',
+        textAlign: 'center',
+    };
+
+
     return (
         <>
         <Header></Header>
+        <div className='invest_section'>
+            <div className='invest_title'>
+                <h1 className='heading'>Invest Daily, Earn Daily</h1>
+                <p className='bio'>Grow your wealth with our daily trading platform. Deposit <br></br> funds and watch your profits accumulate daily.</p>
+                <button>Get Started</button>
+            </div>
+            <div className='invest_animation'>
+                <div style={containerStyle}>
+                    <Lottie options={defaultOptions}/>
+                </div>
+            </div>
+        </div>
         <Footer></Footer>
         </>
     ); 
 }
 
 
-export default investingTrading;
+export default InvestingTrading;
