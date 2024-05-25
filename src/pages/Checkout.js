34d878b96ab8fd508f7948b2050d9f42bf355729
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-05-25 11:42:54 +0530

    added routes to checkout and dashboard

---
+++ src/pages/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+
+function Checkout()
+{
+  
+}
+
+
+export default Checkout;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+
+function Dashboard()
+{
+  
+}
+
+export default Dashboard;
