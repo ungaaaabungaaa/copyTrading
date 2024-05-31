86de6f5cb869f0174893ef5f5842494cdbed17cc
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-05-31 15:20:27 +0530

    added verification process as well

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,7 +8,8 @@ import Lottie from 'react-lottie';
 import animationData from '../assets/Animations/profile.json';
 import { useAuth } from '../contexts/AuthContext';
 import { db } from '../firebase';
-import '../styles/hero2.css'
+import '../styles/hero2.css';
+
 import { useNavigate } from 'react-router-dom';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { useSnackbar } from '../components/Snackbar';
